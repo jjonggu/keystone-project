237741ed4a975f501c43e778c2b1774eeeb5ebf1
jjonggu <whdrnal33@kakao.com>
2025-12-06 22:10:11 +0900

    types 폴더 추가 및 theme.ts 타입스크립트 파일 정의 ts로 tsx = React + TypeScript는 ts 파일로 정의

---
+++ react-app/src/types/theme.ts
@@ -0,0 +1,11 @@
+export interface Theme {
+  themeId: number;
+  themeName: string;
+  themeDescription: string;
+  difficulty: number;
+  minPerson: number;
+  playTime: number;
+  pricePerPerson: number;
+  imageUrl: string;
+  isActive: boolean;
+}
